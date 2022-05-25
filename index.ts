0299b72fff3eb31a0d9f6ba3508dabe8acd35dfc
Roman <99881880+degtyarev-rrr@users.noreply.github.com>
2022-05-25 17:37:49 +0300

    feat: add solution for task_07

---
+++ index.ts
@@ -0,0 +1,34 @@
+// Функция принимает массив meetups,
+// и возвращает суммарное количество человек, находящихся на активных митапах
+ 
+// Пример: 
+// const meetups = [
+//   { name: 'JavaScript', isActive: true, members: 100 },
+//   { name: 'Angular', isActive: true, members: 900 },
+//   { name: 'Node', isActive: false, members: 600 },
+//   { name: 'React', isActive: true, members: 500 },
+// ];
+// membersOnActiveMeetups(meetups); // 1500
+
+type Meetup = {
+  name: string;
+  isActive: boolean;
+  members: number;
+}
+
+const membersOnActiveMeetups = (meetups: Meetup[]): number => {
+  return meetups.reduce((prev, item) => {
+    if(item.isActive) return item.members + prev;
+
+    return prev;
+  }, 0)
+}
+
+const meetups = [
+  { name: 'JavaScript', isActive: true, members: 100 },
+  { name: 'Angular', isActive: true, members: 900 },
+  { name: 'Node', isActive: false, members: 600 },
+  { name: 'React', isActive: true, members: 500 },
+];
+
+console.log(membersOnActiveMeetups(meetups)); // 1500
