baaf2f454e06c4e28fb0d394e0222d332fbb87a6
Roman Degterov <wize.arbik@gmail.com>
2022-05-26 13:11:20 +0300

    feat: add solution for task_09

---
+++ index.ts
@@ -0,0 +1,28 @@
+// const obj = {
+//   id: 0,
+//   name: 'Obj-name',
+//   // ...
+// };
+
+// console.log(`Name: ${obj}`); 		// Name: Obj-name
+// console.log(+obj);            		// 0
+// console.log(obj + 10);        		// 10
+
+
+const obj = {
+  id: 0,
+  name: 'Obj-name',
+  
+  toString() {
+    return this.name;
+  },
+  
+  valueOf() {
+    return this.id;
+  }
+};
+
+
+console.log(`Name: ${obj}`); 		// Name: Obj-name
+console.log(+obj);            		// 0
+console.log(obj + 10);        		// 10
