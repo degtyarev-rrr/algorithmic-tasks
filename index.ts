eaa9bce9c4ac0c0ea2a0bc8fc4dc757c4b97c6a9
Roman Degterov <wize.arbik@gmail.com>
2022-05-26 16:41:12 +0300

    feat: add solution for task_20

---
+++ index.ts
@@ -0,0 +1,22 @@
+// Создать функцию с именем divisors / Divisors, которая принимает целое число n> 1 и возвращает массив со всеми делителями целого числа (кроме 1 и самого числа), от наименьшего до наибольшего. Если число простое, вернуть строку '(integer) is prime'.
+
+// Примеры:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+const divisors = (num: number): number[] | string => {
+  const answer = [];
+  
+  for (let i = 2; i < num; i++) {
+    if(num % i === 0) answer.push(i);
+  }
+
+  if(answer.length === 0) return `${num} is prime`;
+
+  return answer;
+}
+
+console.log(divisors(12)); // should return [2,3,4,6]
+console.log(divisors(25)); // should return [5]
+console.log(divisors(13)); // should return "13 is prime"
