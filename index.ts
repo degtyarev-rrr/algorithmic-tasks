eb557b7b712e95e9308aa2a6b514ccfc19b05212
Roman Degterov <wize.arbik@gmail.com>
2022-05-27 11:57:30 +0300

    feat: add solution for task_23

---
+++ index.ts
@@ -0,0 +1,20 @@
+// Вернуть средний символ слова. Если длина слова нечетная, вернуть средний символ. Если длина слова четная, вернуть 2 средних символа.
+
+// Примеры:
+// getMiddle("test") 	// should return "es"
+// getMiddle("testing") 	// should return "t"
+// getMiddle("middle") 	// should return "dd"
+// getMiddle("A") 	// should return "A"
+
+const getMiddle = (str: string): string => {
+  if(str.length % 2 === 0) {
+    return str[str.length / 2 - 1] + str[str.length / 2];
+  }
+
+  return str[Math.floor(str.length / 2)]
+}
+
+console.log(getMiddle("test")) 	// should return "es"
+console.log(getMiddle("testing")) 	// should return "t"
+console.log(getMiddle("middle")) 	// should return "dd"
+console.log(getMiddle("A")) 	// should return "A"
